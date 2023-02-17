cb93d0a73b451aec7439fc54a466b2ba8a0bf669
Saniya Mathewkutty <S00223097@atu.ie>
2023-02-17 00:00:42 +0000

    Implemented post title, link and vote elements

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit 
+{
+  // Declare variableNames:type
+  votes:number;
+  title:string;
+  link:string;
+
+  // Initialise the variables in the constructor
+  constructor() {
+    this.title="Angular";
+    this.link="https://angular.io";
+    this.votes=10;
+   }
+
+  ngOnInit() { }
 
 }
